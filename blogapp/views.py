85a063f792f2ad46ecc562959bfcfa42234e451a
hmtcelik <abhmtcelik@gmail.com>
2021-12-08 13:19:46 +0300

    tests something

--- blogapp/views.py
+++ blogapp/views.py
@@ -4,7 +4,13 @@ from .models import Post
 
 class BlogView(TemplateView):
     template_name = 'blog.html'
+    context_object_name = 'all_posts'
+    model = Post
+    
+    def get_queryset(self):
+        return Post.objects.all()
     
 class PostView(TemplateView):
     template_name = 'post.html'
+    model = Post
     
